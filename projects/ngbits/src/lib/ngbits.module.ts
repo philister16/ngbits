f22515183f30c07045e639d4238d69e200dc9b3f
Philipp Nueesch <philipp.nueesch@gmail.com>
2020-05-01 17:05:06 +0800

    Import all modules to root ngbits module

--- projects/ngbits/src/lib/ngbits.module.ts
+++ projects/ngbits/src/lib/ngbits.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { NgbitFlashModule } from './ngbit-flash/ngbit-flash.module';
+import { NgbitIndicatorButtonModule } from './ngbit-indicator-button/ngbit-indicator-button.module';
+import { NgbitLoadingButtonModule } from './ngbit-loading-button/ngbit-loading-button.module';
+import { NgbitDropzoneModule } from './ngbit-dropzone/ngbit-dropzone.module';
 
 @NgModule({
   declarations: [],
   imports: [
-    NgbitFlashModule
+    NgbitFlashModule,
+    NgbitIndicatorButtonModule,
+    NgbitLoadingButtonModule,
+    NgbitDropzoneModule
   ],
   exports: []
 })

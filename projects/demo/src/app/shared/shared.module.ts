c14afaaa7de96c1d3d4ac36a7ca266e43288d7d8
Philipp Nueesch <philipp.nueesch@gmail.com>
2020-05-11 21:18:53 +0800

    Style docs

--- projects/demo/src/app/shared/shared.module.ts
+++ projects/demo/src/app/shared/shared.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApiTableComponent } from './api-table/api-table.component';
-import { DemoBoxComponent } from './demo-box/demo-box.component';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
 import { RouterModule } from '@angular/router';
 
 const ASSETS = [
   ApiTableComponent,
-  DemoBoxComponent,
   MainLayoutComponent
 ];
 
